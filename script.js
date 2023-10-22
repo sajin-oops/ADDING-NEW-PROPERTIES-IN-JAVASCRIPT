2c25dc07601665fa866e33fe361ea4b2d4fe75c8
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-22 20:50:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let item = {
+  name:"iphone",
+  price:50000,
+  quantity:2
+}
+console.log(item)
+// - output -{name: 'iphone', price: 50000, quantity: 2}
+
+//ANOTHER WAY OF CREATING OBJECT
+
+let item2 = new Object();
+item2.name = "macbook pro"
+item2.processor = "m2"
+item2.quantity=1
+console.log(item2)
+// output - {name: 'macbook pro', processor: 'm2', quantity: 1}
+
+
+//-accessing object
+// - dot notation
+console.log(item.name)
+console.log(item2.processor)
+console.log(item.price)
+console.log(item.quantity) 
+
+item.quantity = 6
+console.log(item.quantity) 
+
+// ADDING NEW PROPERTIES
+
+item.owner = "sajin"
+console.log(item)
+// - {name: 'iphone', price: 50000, quantity: 6, owner: 'sajin'}
+
+
